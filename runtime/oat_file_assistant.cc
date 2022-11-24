4d515a9581776d1a16b6f8205405f5f2ef373fdc
Jiakai Zhang <jiakaiz@google.com>
2022-11-24 17:27:07 +0000

    Fix OatFileAssistant to return kNoDexOptNeeded when dex file is missing.
    Before this change, OatFileAssistant correctly returns kNoDexOptNeeded
    when both the dex file and the artifacts are missing, but it
    unexpectedly returns kDex2OatForFilter when the dex file is missing
    while the artifacts exist. This change fixes this.
    
    Bug: 229268202
    Test: m test-art-host-gtest-art_runtime_tests
    Change-Id: Id526bb73385e3a9313333e70019c6d56c0a385f6
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- runtime/oat_file_assistant.cc
+++ runtime/oat_file_assistant.cc
@@ -391,8 +391,7 @@ bool OatFileAssistant::DexChecksumUpToDate(const VdexFile& file, std::string* er
   ScopedTrace trace("DexChecksumUpToDate(vdex)");
   const std::vector<uint32_t>* required_dex_checksums = GetRequiredDexChecksums(error_msg);
   if (required_dex_checksums == nullptr) {
-    // TODO(jiakaiz): Change to false.
-    return true;
+    return false;
   }
   if (required_dex_checksums->empty()) {
     LOG(WARNING) << "Required dex checksums not found. Assuming dex checksums are up to date.";
@@ -428,8 +427,7 @@ bool OatFileAssistant::DexChecksumUpToDate(const OatFile& file, std::string* err
   ScopedTrace trace("DexChecksumUpToDate(oat)");
   const std::vector<uint32_t>* required_dex_checksums = GetRequiredDexChecksums(error_msg);
   if (required_dex_checksums == nullptr) {
-    // TODO(jiakaiz): Change to false.
-    return true;
+    return false;
   }
   if (required_dex_checksums->empty()) {
     LOG(WARNING) << "Required dex checksums not found. Assuming dex checksums are up to date.";
